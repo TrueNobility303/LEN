be464dfe1d13bc8c4ae7d8776fa5cfd5f9c3ba28
Lesi Chen <78390095+TrueNobility303@users.noreply.github.com>
2024-03-26 10:54:18 +0800

    Add files via upload

---
+++ Fairness.py
@@ -0,0 +1,223 @@
+# Codes adapted from
+
+import argparse
+import numpy as np 
+import matplotlib.pyplot as plt 
+import random 
+import time 
+import torch 
+from scipy.io import loadmat, savemat
+
+parser = argparse.ArgumentParser(description='Minimax Optimization.')
+parser.add_argument('--training_time',  type=float, default=10.0, help='total training time')
+parser.add_argument('--dataset', type=str, default='adult', help='dataset: adult / lawschool') 
+parser.add_argument('--M', type=float, default=10.0, help='parameter M for ARE')
+parser.add_argument('--LazyM', type=float, default=10.0, help='parameter M for LazyARE')
+parser.add_argument('--seed',  type=int, default=42, help='seed of random number')
+parser.add_argument('--eta', type=float, default=1e-1, help='stepsize of extra gradient')
+parser.add_argument('--max_iter',  type=int, default=10, help='maximum iteration for cubic subsolver')
+parser.add_argument('--eps',  type=float, default=1e-8, help='precision for cubic subsolver')
+
+# Parameters in problem setting
+parser.add_argument('--lamb',  type=float, default=0.0)
+parser.add_argument('--gamma',  type=float, default=0.0)
+parser.add_argument('--beta',  type=float, default=0.5)
+args = parser.parse_args()
+
+device=torch.device("cuda:0")
+
+def load_adult():
+    m = loadmat('./Data/a9a.mat')
+    A0=np.array(m['A']).astype("float")
+    b=np.array(m['b']).astype("float")
+    c=A0[:,71]
+    A=np.hstack((A0[:,:71],A0[:,72:]))
+    c[np.where(c==0)[0]]=-1
+    c=c.reshape(A0.shape[0],1)
+    A=torch.from_numpy(A).double()
+    b=torch.from_numpy(b).double()
+    c=torch.from_numpy(c).double()
+    return A,b,c
+
+def load_lawschool():
+    m = loadmat('./Data/LSTUDENT_DATA1.mat')
+    A=np.array(m['A']).astype("float")
+    b=np.array(m['b']).astype("float")
+    b[np.where(b==0)[0]]=-1
+    c=np.array(m['c']).astype('float')
+    c[np.where(c==0)[0]]=-1
+    A=torch.from_numpy(A).double()
+    b=torch.from_numpy(b).double()
+    c=torch.from_numpy(c).double()
+    return A,b,c 
+
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+set_seed(args.seed)
+
+class Fairness_Learning:
+    def __init__(self,A,b,c,lamb,gamma,beta):
+        self.m,self.dim=A.shape
+        self.d=self.dim+1
+        self.gamma=gamma
+        self.beta=beta
+        self.A=A.to(device)
+        self.lamb=lamb
+        self.b=b.to(device)
+        self.c=c.to(device)
+
+
+    def GDA_field(self,z):
+        x=z[:self.dim]
+        y=z[self.dim]
+        Ax = self.A @ x
+        p1=1/(1+torch.exp(self.b*Ax))
+        p2=1/(1+torch.exp(self.c*(Ax)*y))
+        gx=-self.A.T@(p1*self.b/self.m)+self.A.T@(self.c*y*p2*self.beta/self.m)+2*self.lamb*x
+        gy=torch.sum((Ax)*self.c*self.beta*p2/self.m)-2*self.gamma*y
+        g=torch.zeros(self.d,1).double().to(device)
+        g[:self.dim]=gx
+        g[self.dim]=-gy
+        return g
+
+    def Jacobian_GDA_field(self,z):
+        x=z[:self.dim]
+        y=z[self.dim]
+        multt=self.A@x
+        p1=1/(1+torch.exp(self.b*multt))
+        p2=1/(1+torch.exp(self.c*(multt)*y))
+        Hxx=self.A.T@(p1*(1-p1)/self.m*self.A)-self.A.T@(p2*(1-p2)*self.beta/self.m*y**2*self.A)+2*self.lamb*torch.eye(self.dim).double().to(device)
+        Hyy=-torch.sum(p2*(1-p2)/self.m*self.beta*(self.A@x)**2)-2*self.gamma
+        Hxy=-self.A.T@((self.A@x)*y*p2*(1-p2)*self.beta/self.m)+self.A.T@(p2*self.beta/self.m*self.c)
+        H=torch.eye(self.d).double().to(device)
+        H[:self.dim,:self.dim]=Hxx
+        H[self.dim,self.dim]=-Hyy
+        H[:self.dim,self.dim]=Hxy.reshape(-1)
+        H[self.dim,:self.dim]=-Hxy.reshape(-1)
+        return H
+
+if args.dataset == 'adult':
+    A,b,c = load_adult()
+elif args.dataset == 'lawschool':
+    A,b,c = load_lawschool()
+else:
+    raise ValueError('should use the correct dataset')
+
+oracle = Fairness_Learning(A,b,c,lamb=args.lamb,gamma=args.gamma,beta=args.beta)
+z0=torch.zeros(oracle.d,1).double().to(device)
+
+# Extra Gradient
+
+def EG(oracle, z0, args):
+    time_lst_EG = []
+    gnorm_lst_EG = []
+    ellapse_time = 0.0
+    z = z0
+    i = 0
+    while True:
+        start = time.time()
+        gz =  oracle.GDA_field(z)
+        z_half = z - args.eta * gz
+        gz_half = oracle.GDA_field(z_half)
+        z = z - args.eta * gz_half
+        end = time.time()
+        ellapse_time += end - start
+        
+        if ellapse_time > args.training_time:
+            break 
+        time_lst_EG.append(ellapse_time)
+
+        gnorm = torch.linalg.norm(gz_half).item()
+        gnorm_lst_EG.append(gnorm)
+
+        if i % 10 == 0:
+            print('EG: Epoch %d |  gradient norm %.4f' % (i, gnorm))
+
+        i = i + 1
+    return time_lst_EG, gnorm_lst_EG
+
+# LazyARE 
+def LazyARE(oracle, z0, args, M, m):
+    time_lst_LazyARE = []
+    gnorm_lst_LazyARE = []
+    ellapse_time = 0.0
+    z = z0
+    z_half = z0
+    i = 0
+    while True:
+        start = time.time()
+        gz =  oracle.GDA_field(z).cdouble()
+
+        # Compute the spectral decomposition of the snapshot Hessian
+        if i % m == 0:
+            Hz = oracle.Jacobian_GDA_field(z)
+            Lambda, Q = torch.linalg.eig(Hz)
+            Lambda = torch.reshape(Lambda, (oracle.d, 1))
+            invQ = torch.linalg.inv(Q)
+
+        # Apply univariate Newton method to solve the cubic sub-problem
+        r = torch.linalg.norm(z_half - z)
+        tilde_gz = invQ @ gz 
+        
+        for j in range(args.max_iter):
+            inverse_reg_Gamma = torch.pow(Lambda + M * r, -1)
+            s = (Q @ (inverse_reg_Gamma * tilde_gz)).real
+            s_norm = torch.linalg.norm(s)
+            varphi = r - s_norm    
+            varphi_prime = 1 + (M / s_norm * tilde_gz.conj().T @ ( torch.pow(inverse_reg_Gamma, 3) * tilde_gz)).real
+
+            r = r - varphi / varphi_prime
+            if torch.abs(varphi) < args.eps:
+                print('Epoch %d | Univariate Newton succeed in iteration %d' % (i, j))
+                break
+        
+        inverse_reg_Gamma = torch.pow(Lambda + M * r, -1)
+        z_half = (z - Q @ ( inverse_reg_Gamma * tilde_gz)).real
+
+        gamma = M * torch.linalg.norm(z_half - z)
+        eta = 1 / gamma 
+
+        gz_half = oracle.GDA_field(z_half)
+        z = z - eta * gz_half
+
+        end = time.time()
+        ellapse_time += end - start
+        if ellapse_time > args.training_time:
+            break
+        time_lst_LazyARE.append(ellapse_time)
+
+        gnorm = torch.linalg.norm(gz_half).item()
+        gnorm_lst_LazyARE.append(gnorm)
+
+        if i % 10 == 0:
+            print('LazyARE (m=%d): Epoch %d  | gradient norm %.4f' % (m, i, gnorm))
+        i = i + 1
+    return time_lst_LazyARE, gnorm_lst_LazyARE
+
+time_lst_ARE, gnorm_lst_ARE = LazyARE(oracle, z0, args, M=args.M, m=1)
+time_lst_EG, gnorm_lst_EG = EG(oracle, z0, args)
+
+d = oracle.d
+time_lst_LazyARE, gnorm_lst_LazyARE = LazyARE(oracle, z0, args, M=args.LazyM, m=d)
+
+# Plot the results time vs. gnorm 
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+plt.rc('font', size=21)
+plt.figure()
+plt.grid()
+plt.yscale('log')
+plt.plot(time_lst_EG, gnorm_lst_EG, '-.b', label='EG', linewidth=3)
+plt.plot(time_lst_ARE, gnorm_lst_ARE, ':r', label='ARE', linewidth=3)
+plt.plot(time_lst_LazyARE, gnorm_lst_LazyARE, '-k', label='LazyARE', linewidth=3)
+plt.legend(fontsize=23, loc='lower right')
+plt.tick_params('x',labelsize=21)
+plt.tick_params('y',labelsize=21)
+plt.ylabel('grad. norm')
+plt.xlabel('time')
+plt.tight_layout()
+plt.savefig('./img/'+  'Fairness.' + args.dataset + '.' + str(args.beta) + '.png')
+plt.savefig('./img/'+  'Fairness.' + args.dataset + '.' + str(args.beta) + '.pdf', format = 'pdf')
---
+++ Synthetic.py
@@ -0,0 +1,339 @@
+import argparse
+import numpy as np 
+import matplotlib.pyplot as plt 
+import random 
+from scipy.optimize import fsolve
+import time 
+
+parser = argparse.ArgumentParser(description='Minimax Optimization.')
+parser.add_argument('--training_time',  type=float, default=10.0, help='total training time')
+parser.add_argument('--n', type=int, default=100, help='size of problem')
+parser.add_argument('--seed',  type=int, default=42, help='seed of random number')
+parser.add_argument('--eta', type=float, default=1e-2, help='stepsize of extra gradient')
+parser.add_argument('--max_iter',  type=int, default=10, help='maximum iteration for cubic subsolver')
+parser.add_argument('--eps',  type=float, default=1e-8, help='precision for cubic subsolver')
+args = parser.parse_args()
+
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+
+set_seed(args.seed)
+
+# Generate the data
+n = args.n
+rho = 1 / (20*n)
+b = 2 * np.random.randint(2, size=(n,1)) - 1
+
+A = np.zeros((n,n))
+for i in range(n-1):
+    A[i,i] = 1 
+    A[i,i+1] = -1
+A[n-1,n-1] = 1
+
+# print(A)
+# print(b)
+
+# Calculate the global saddle points
+
+
+d = 2 * n
+
+x_star = np.linalg.inv(A) @ b
+y_star = - rho / 2 * np.linalg.norm(x_star) * np.linalg.inv(A.T) @ x_star
+
+z_star = np.zeros((d,1))
+z_star[:n] = x_star
+z_star[n:] = y_star
+z0 = np.zeros((d,1))
+
+# Define some helper functions
+
+def GDA_field(z):
+    x = z[:n]
+    y = z[n:]
+    gx = rho / 2 * np.linalg.norm(x) * x + A.T @ y 
+    gy = A @ x - b
+    g = np.zeros_like(z)
+    g[:n] = gx  
+    g[n:] = -gy
+    return g
+
+# g_star = GDA_field(z_star)
+# print(np.linalg.norm(g_star))
+
+def Jacobian_GDA_field(z):
+    x = z[:n]
+    y = z[n:]
+    nx = np.linalg.norm(x)
+    
+    H = np.zeros((d,d))
+    if nx > 0:
+        H[:n,:n] = rho / 2 * nx * np.eye(n) + rho / 2 * x @ x.T / nx 
+    H[:n,n:] = A.T
+    H[n:,:n] = -A
+    return H
+
+# Extra Gradient
+
+time_lst_EG = []
+dist_lst_EG = []
+gnorm_lst_EG = []
+ellapse_time = 0.0
+z = z0
+i = 0
+while True:
+    start = time.time()
+    gz =  GDA_field(z)
+    z_half = z - args.eta * gz
+    gz_half = GDA_field(z_half)
+    z = z - args.eta * gz_half
+    end = time.time()
+    ellapse_time += end - start
+    
+    if ellapse_time > args.training_time:
+        break 
+    time_lst_EG.append(ellapse_time)
+
+    dist = np.linalg.norm(z - z_star)
+    dist_lst_EG.append(dist)
+    gnorm = np.linalg.norm(gz_half)
+    gnorm_lst_EG.append(gnorm)
+
+    if i % 10 == 0:
+        print('EG: Epoch %d | distance to saddle %.4f | gradient norm %.4f' % (i, dist, gnorm))
+
+    i = i + 1
+
+# LazyARE 
+m = d
+M = rho * m
+
+time_lst_LazyARE = []
+dist_lst_LazyARE = []
+gnorm_lst_LazyARE = []
+ellapse_time = 0.0
+
+z = z0
+z_half = z0
+i = 0
+while True:
+    start = time.time()
+    gz =  GDA_field(z)
+
+    # Compute the spectral decomposition of the snapshot Hessian
+    if i % m == 0:
+        Hz = Jacobian_GDA_field(z)
+        Lambda, Q = np.linalg.eig(Hz)
+        Lambda = np.expand_dims(Lambda, axis=-1)
+        invQ = np.linalg.inv(Q)
+
+        # QH = np.conj(Q.T)
+        # print(QH - invQ)
+
+        # H = Q @ (Lambda * invQ)
+
+    # Apply univariate Newton method to solve the cubic sub-problem
+    r = np.linalg.norm(z_half - z)
+    tilde_gz = invQ @ gz 
+    
+    for j in range(args.max_iter):
+        inverse_reg_Gamma = np.power(Lambda + M * r, -1, dtype=complex)
+        s = (Q @ (inverse_reg_Gamma * tilde_gz)).real
+        s_norm = np.linalg.norm(s)
+        varphi = r - s_norm    
+        varphi_prime = 1 + (M / s_norm * tilde_gz.conj().T @ ( np.power(inverse_reg_Gamma, 3, dtype=complex) * tilde_gz)).real
+
+        r = r - varphi / varphi_prime
+        if np.abs(varphi) < args.eps:
+            print('Epoch %d | Univariate Newton succeed in iteration %d' % (i, j))
+            break
+    
+    inverse_reg_Gamma = np.power(Lambda + M * r, -1, dtype=complex )
+    z_half = (z - Q @ ( inverse_reg_Gamma * tilde_gz)).real
+
+    gamma = M * np.linalg.norm(z_half - z)
+    eta = 1 / gamma 
+
+    gz_half = GDA_field(z_half)
+    z = z - eta * gz_half
+
+    end = time.time()
+    ellapse_time += end - start
+    if ellapse_time > args.training_time:
+        break
+    time_lst_LazyARE.append(ellapse_time)
+
+    dist = np.linalg.norm(z_half - z_star)
+    dist_lst_LazyARE.append(dist)
+    gnorm = np.linalg.norm(gz_half)
+    gnorm_lst_LazyARE.append(gnorm)
+
+    if i % 10 == 0:
+        print('LazyARE: Epoch %d | distance to saddle %.4f | gradient norm %.4f' % (i, dist, gnorm))
+    i = i + 1
+
+
+# ARE (LazyARE with m=1) 
+m = 1
+M = rho * m
+
+time_lst_ARE = []
+dist_lst_ARE = []
+gnorm_lst_ARE = []
+ellapse_time = 0.0
+
+z = z0
+z_half = z0
+i = 0
+while True:
+    start = time.time()
+    gz =  GDA_field(z)
+
+    # Compute the spectral decomposition of the snapshot Hessianplt.rc('font', size=21)
+    if i % m == 0:
+        Hz = Jacobian_GDA_field(z)
+        Lambda, Q = np.linalg.eig(Hz)
+        Lambda = np.expand_dims(Lambda, axis=-1)
+        invQ = np.linalg.inv(Q)
+
+        # H = Q @ (Lambda * invQ)
+
+    # Apply univariate Newton method to solve the cubic sub-problem
+    r = np.linalg.norm(z_half - z)
+    tilde_gz = invQ @ gz 
+    
+    for j in range(args.max_iter):
+        inverse_reg_Gamma = np.power(Lambda + M * r, -1, dtype=complex)
+        s = (Q @ (inverse_reg_Gamma * tilde_gz)).real
+        s_norm = np.linalg.norm(s)
+        varphi = r - s_norm    
+        varphi_prime = 1 + (M / s_norm * tilde_gz.conj().T @ ( np.power(inverse_reg_Gamma, 3, dtype=complex) * tilde_gz)).real
+
+        r = r - varphi / varphi_prime
+        if np.abs(varphi) < args.eps:
+            print('Epoch %d | Univariate Newton succeed in iteration %d' % (i, j))
+            break
+    
+    inverse_reg_Gamma = np.power(Lambda + M * r, -1, dtype=complex )
+    z_half = (z - Q @ ( inverse_reg_Gamma * tilde_gz)).real
+
+    gamma = M * np.linalg.norm(z_half - z)
+    eta = 1 / gamma 
+
+    gz_half = GDA_field(z_half)
+    z = z - eta * gz_half
+
+    end = time.time()
+    ellapse_time += end - start
+    if ellapse_time > args.training_time:
+        break
+    time_lst_ARE.append(ellapse_time)
+
+    dist = np.linalg.norm(z_half - z_star)
+    dist_lst_ARE.append(dist)
+    gnorm = np.linalg.norm(gz_half)
+    gnorm_lst_ARE.append(gnorm)
+
+    if i % 10 == 0:
+        print('ARE: Epoch %d | distance to saddle %.4f | gradient norm %.4f' % (i, dist, gnorm))
+    i = i + 1
+
+# # ARE with fsolve 
+# M = rho
+
+# time_lst_ARE = []
+# dist_list_ARE = []
+# gnorm_list_ARE = []
+
+# Hess_time = 0.0
+# grad_time = 0.0
+# ellapse_time = 0.0
+
+# z = z0
+# z_warmstart = z0
+# i = 0
+# while True:
+#     start = time.time()
+#     gz =  GDA_field(z)
+#     Hz = Jacobian_GDA_field(z)
+#     def func(z_new):
+#         z_new = np.expand_dims(z_new, axis=-1)
+#         f = gz - (Hz + M * np.linalg.norm(z - z_new)) @ (z - z_new)
+#         f = f.squeeze(axis=-1)
+#         return f
+#     z_half = fsolve(func, z_warmstart)
+#     z_half = np.expand_dims(z_half, axis=-1)
+#     z_warmstart = z_half
+#     gamma = M * np.linalg.norm(z_half - z)
+#     eta = 1 / gamma 
+    
+#     middle = time.time()
+#     Hess_time += middle - start
+
+#     gz_half = GDA_field(z_half)
+#     z = z - eta * gz_half
+#     end = time.time()
+#     grad_time += end - middle
+#     ellapse_time += end - start
+#     if ellapse_time > args.training_time:
+#         break
+#     time_lst_ARE.append(ellapse_time)
+
+#     dist = np.linalg.norm(z_half - z_star)
+#     dist_list_ARE.append(dist)
+#     gnorm = np.linalg.norm(gz_half)
+#     gnorm_list_ARE.append(gnorm)
+
+#     if i % 10 == 0:
+#         print('ARE: Epoch %d | distance to saddle %.4f | gradient norm %.4f' % (i, dist, gnorm))
+
+#     i = i + 1
+
+# cost_ratio = Hess_time / grad_time
+
+# print('============== Finish ARE Training====================')
+# print('Problem dimension: %d | Cost of Hess / Cost of Grad %.2f ' % (d, cost_ratio))
+
+
+# Plot the results
+
+# time vs. gnorm 
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+plt.rc('font', size=21)
+plt.figure()
+plt.grid()
+plt.yscale('log')
+plt.plot(time_lst_EG, gnorm_lst_EG, '-.b', label='EG', linewidth=3)
+plt.plot(time_lst_ARE, gnorm_lst_ARE, ':r', label='ARE', linewidth=3)
+plt.plot(time_lst_LazyARE, gnorm_lst_LazyARE, '-k', label='LazyARE', linewidth=3)
+plt.legend(fontsize=23, loc='lower right')
+plt.tick_params('x',labelsize=21)
+plt.tick_params('y',labelsize=21)
+plt.ylabel('grad. norm')
+plt.xlabel('time')
+plt.tight_layout()
+plt.savefig('./img/'+  'Synthetic.' +  str(args.n) + '.gnorm.png')
+plt.savefig('./img/'+  'Synthetic.' +  str(args.n) + '.gnorm.pdf', format = 'pdf')
+
+# time vs. dist graph
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+plt.rc('font', size=21)
+plt.figure()
+plt.grid()
+plt.yscale('log')
+plt.plot(time_lst_EG, dist_lst_EG, '-.b', label='EG', linewidth=3)
+plt.plot(time_lst_ARE, dist_lst_ARE, ':r', label='ARE', linewidth=3)
+plt.plot(time_lst_LazyARE, dist_lst_LazyARE, '-k', label='LazyARE', linewidth=3)
+plt.legend(fontsize=23, loc='lower right')
+plt.tick_params('x',labelsize=21)
+plt.tick_params('y',labelsize=21)
+plt.ylabel('dist. to saddle')
+plt.xlabel('time')
+plt.tight_layout()
+plt.savefig('./img/'+  'Synthetic.' +  str(args.n) + '.dist.png')
+plt.savefig('./img/'+  'Synthetic.' +  str(args.n) + '.dist.pdf', format = 'pdf')
+
+
