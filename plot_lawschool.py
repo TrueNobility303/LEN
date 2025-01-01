86c10d54d2878823d497698d9970ebe376e2cfdb
Lesi Chen <78390095+TrueNobility303@users.noreply.github.com>
2025-01-01 09:32:06 +0800

    Update plot_lawschool.py

--- plot_lawschool.py
+++ plot_lawschool.py
@@ -8,15 +8,15 @@ with open('./result/'+  'Fairness.' + dataset + '.gnorm_lst_EG.pkl', 'rb') as f:
     gnorm_lst_EG = pickle.load(f)
 
 with open('./result/'+  'Fairness.' + dataset + '.time_lst_EG2.pkl', 'rb') as f:
-    time_lst_LEAN_1 = pickle.load(f) 
+    time_lst_LEN_1 = pickle.load(f) 
 with open('./result/'+  'Fairness.' + dataset + '.gnorm_lst_EG2.pkl', 'rb') as f:
-    gnorm_lst_LEAN_1 = pickle.load(f)
+    gnorm_lst_LEN_1 = pickle.load(f)
 
 with open('./result/'+  'Fairness.' + dataset + '.time_lst_LEN.pkl', 'rb') as f:
-    time_lst_LEAN_10 = pickle.load(f)
+    time_lst_LEN_10 = pickle.load(f)
 
 with open('./result/'+  'Fairness.' + dataset + '.gnorm_lst_LEN.pkl', 'rb') as f:
-    gnorm_lst_LEAN_10 = pickle.load(f)
+    gnorm_lst_LEN_10 = pickle.load(f)
 
 import matplotlib.pyplot as plt 
 
@@ -28,8 +28,8 @@ plt.figure()
 plt.grid()
 plt.yscale('log')
 plt.plot(time_lst_EG, gnorm_lst_EG, '-.b', label='EG', linewidth=3)
-plt.plot(time_lst_LEAN_1, gnorm_lst_LEAN_1, ':r', label='EG-2', linewidth=3)
-plt.plot(time_lst_LEAN_10, gnorm_lst_LEAN_10, '-k', label='LEN', linewidth=3)
+plt.plot(time_lst_LEN_1, gnorm_lst_LEN_1, ':r', label='EG-2', linewidth=3)
+plt.plot(time_lst_LEN_10, gnorm_lst_LEN_10, '-k', label='LEN', linewidth=3)
 plt.legend(fontsize=23, loc='lower right')
 plt.tick_params('x',labelsize=21)
 plt.tick_params('y',labelsize=21)
